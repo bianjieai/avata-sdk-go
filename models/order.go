65763e8b84c8d0d5facdd598a5114e7425ecb0bf
shimin <shimin@bianjie.ai>
2023-01-19 15:46:56 +0800

    add explain

--- models/order.go
+++ models/order.go
@@ -9,45 +9,45 @@ const (
 
 // CreateOrderReq 购买能量值/业务费接口请求参数
 type CreateOrderReq struct {
-	Account   string `json:"account"`
-	Amount    int    `json:"amount"`
-	OrderType string `json:"order_type"`
-	OrderId   string `json:"order_id"`
+	Account   string `json:"account"`    // 链账户地址
+	Amount    int    `json:"amount"`     // 购买金额 ，只能购买整数元金额；单位：分
+	OrderType string `json:"order_type"` // 充值类型：gas：能量值；business：业务费；Enum: "gas" "business"
+	OrderId   string `json:"order_id"`   // 自定义订单流水号，必需且仅包含数字、下划线及英文字母大/小写
 }
 
 // OrderRes 购买能量值/业务费接口返回值/批量购买能量值接口返回值
 type OrderRes struct {
 	Data struct {
-		OrderId string `json:"order_id"`
+		OrderId string `json:"order_id"` // 交易流水号（用户发起交易时传入的交易流水号)
 	} `json:"data"`
 }
 
 // QueryOrdersReq 查询能量值/业务费购买结果列表接口请求参数
 type QueryOrdersReq struct {
-	Offset    string `json:"offset,omitempty"`
-	Limit     string `json:"limit,omitempty"`
-	Status    string `json:"status,omitempty"`
-	StartDate string `json:"start_date,omitempty"`
-	EndDate   string `json:"end_date,omitempty"`
-	SortBy    string `json:"sort_by,omitempty"`
+	Offset    string `json:"offset,omitempty"`     // 游标，默认为 0
+	Limit     string `json:"limit,omitempty"`      // 每页记录数，默认为 10，上限为 50
+	Status    string `json:"status,omitempty"`     // 订单状态：success 充值成功 / failed 充值失败 / pending 正在充值
+	StartDate string `json:"start_date,omitempty"` // 充值订单创建日期范围 - 开始，yyyy-MM-dd（UTC 时间）
+	EndDate   string `json:"end_date,omitempty"`   // 充值订单创建日期范围 - 结束，yyyy-MM-dd（UTC 时间）
+	SortBy    string `json:"sort_by,omitempty"`    // 排序规则：DATE_ASC / DATE_DESC，默认为 DATE_DESC
 }
 
 // QueryOrdersRes 查询能量值/业务费购买结果列表接口返回值
 type QueryOrdersRes struct {
 	Data struct {
-		Offset     int `json:"offset"`
-		Limit      int `json:"limit"`
-		TotalCount int `json:"total_count"`
+		Offset     int `json:"offset"`      // 游标
+		Limit      int `json:"limit"`       // 每页记录数
+		TotalCount int `json:"total_count"` // 总记录数
 		OrderInfos []struct {
-			OrderId    string `json:"order_id"`
-			Status     string `json:"status"`
-			Message    string `json:"message"`
-			Account    string `json:"account"`
-			Amount     string `json:"amount"`
-			Number     string `json:"number"`
-			CreateTime string `json:"create_time"`
-			UpdateTime string `json:"update_time"`
-			OrderType  string `json:"order_type"`
+			OrderId    string `json:"order_id"`    // 订单流水号
+			Status     string `json:"status"`      // 订单状态：success 充值成功 / failed 充值失败 / pending 正在充值
+			Message    string `json:"message"`     // 订单失败的错误描述信息
+			Account    string `json:"account"`     // 链账户地址 （调用「批量购买能量值」接口不展示此字段）
+			Amount     string `json:"amount"`      // 充值金额，为整数元金额；单位：分 （调用「批量购买能量值」接口不展示此字段）
+			Number     string `json:"number"`      // 充值的数量，充值 gas 该值单位为 ugas，充值业务费单位为分（调用「批量购买能量值」接口不展示此字段）
+			CreateTime string `json:"create_time"` // 创建时间（UTC 时间）
+			UpdateTime string `json:"update_time"` // 最后操作时间（UTC 时间）
+			OrderType  string `json:"order_type"`  // 订单类型，gas / business
 		} `json:"order_infos"`
 	} `json:"data"`
 }
@@ -55,26 +55,26 @@ type QueryOrdersRes struct {
 // QueryOrderRes 查询能量值/业务费购买结果接口返回值
 type QueryOrderRes struct {
 	Data struct {
-		OrderId    string `json:"order_id"`
-		Status     string `json:"status"`
-		Message    string `json:"message"`
-		Account    string `json:"account"`
-		Amount     string `json:"amount"`
-		Number     string `json:"number"`
-		CreateTime string `json:"create_time"`
-		UpdateTime string `json:"update_time"`
-		OrderType  string `json:"order_type"`
+		OrderId    string `json:"order_id"`    // 订单流水号
+		Status     string `json:"status"`      // 订单状态：success 充值成功 / failed 充值失败 / pending 正在充值
+		Message    string `json:"message"`     // 订单失败的错误描述信息
+		Account    string `json:"account"`     // 链账户地址 （调用「批量购买能量值」接口不展示此字段）
+		Amount     string `json:"amount"`      // 充值金额，为整数元金额；单位：分 （调用「批量购买能量值」接口不展示此字段）
+		Number     string `json:"number"`      // 充值的数量，充值 gas 该值单位为 ugas，充值业务费单位为分（调用「批量购买能量值」接口不展示此字段）
+		CreateTime string `json:"create_time"` // 创建时间（UTC 时间）
+		UpdateTime string `json:"update_time"` // 最后操作时间（UTC 时间）
+		OrderType  string `json:"order_type"`  // 订单类型，gas / business
 	} `json:"data"`
 }
 
 // BatchCreateOrderReq 批量购买能量值接口请求参数
 type (
 	BatchCreateOrderReq struct {
-		List    []List `json:"list"`
-		OrderId string `json:"order_id"`
+		List    []List `json:"list"`     // 充值信息
+		OrderId string `json:"order_id"` // 自定义订单流水号，必需且仅包含数字、下划线及英文字母大/小写
 	}
 	List struct {
-		Account string `json:"account"`
-		Amount  int    `json:"amount"`
+		Account string `json:"account"` // 链账户地址
+		Amount  int    `json:"amount"`  // 购买金额 ，只能购买整数元金额；单位：分
 	}
 )
--- models/record.go
+++ models/record.go
@@ -6,13 +6,13 @@ const (
 
 // CreateRecordReq 数字作品存证接口请求参数
 type CreateRecordReq struct {
-	IdentityType int    `json:"identity_type,omitempty"`
-	IdentityName string `json:"identity_name,omitempty"`
-	IdentityNum  string `json:"identity_num,omitempty"`
-	Type         int    `json:"type"`
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	Hash         string `json:"hash"`
-	HashType     int    `json:"hash_type"`
-	OperationId  string `json:"operation_id"`
+	IdentityType int    `json:"identity_type,omitempty"` // 存证主体；1:个人；2:企业
+	IdentityName string `json:"identity_name,omitempty"` // 个人姓名或企业名称，规范如下：个人姓名：长度限制 1-16 个字符（UTF-8 编码），首字符不能是特殊符号；企业名称：长度限制 1-50 个字符（UTF-8 编码），首字符不能是特殊符号；未传入存证主体字段时，不支持此字段；传入存证主体字段时，此字段必填
+	IdentityNum  string `json:"identity_num,omitempty"`  // 个人为身份证号码，企业为统一社会信用代码； 未传入存证主体字段时，不支持此字段；传入存证主体字段时，此字段选填
+	Type         int    `json:"type"`                    // 作品类型: Enum: 1 2 3 4 5 6 7 8 9 10 11 12 13 14，具体参考接口文档
+	Name         string `json:"name"`                    // 作品名称
+	Description  string `json:"description"`             // 作品描述
+	Hash         string `json:"hash"`                    // 作品哈希；将单个作品源文件使用单向散列函数（如 MD5，SHA 等）进行一次 Hash 计算；将多个作品源文件分别进行一次 Hash 计算，再将得到的 Hash 值进行二次 Hash 计算
+	HashType     int    `json:"hash_type"`               // 作品哈希类型 1:其它； 2:SHA256；3:MD5；4:SHA256-PFV；
+	OperationId  string `json:"operation_id"`            // 操作 ID，保证幂等性，避免重复请求，保证对于同一操作发起的一次请求或者多次请求的结果是一致的；由接入方生成的、针对每个 Project ID 唯一的、不超过 64 个大小写字母、数字、-、下划线的字符串
 }
