104fc4b456203df44a5b49c3bb6ab73ed3e32a3f
deadfiish <94030704+deadfiish@users.noreply.github.com>
2023-09-04 18:12:15 +0800

    update callback (#18)
    * fix callback
    
    Co-authored-by: shimin <shimin@bianjie.ai>

--- tests/call_back_test.go
+++ tests/call_back_test.go
@@ -11,16 +11,24 @@
 package tests
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/bianjieai/avata-sdk-go/utils"
 )
 
-//回调服务
-func CallBack(r *http.Request) string {
+// 回调服务
+func CallBack(r *http.Request, w http.ResponseWriter) error {
+	// 定义一个闭包函数，用于处理回调操作
+	app := func(ctx context.Context, a *http.Request) {
+		// 业务逻辑
+	}
 	//result 需要返回给消息推送端
-	result := utils.OnCallBack("", "", "", r, func() {
-
-	})
-	return result
+	err := utils.OnCallBack(context.Background(), utils.APIVersionV1, "1", "", r, w, app)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
